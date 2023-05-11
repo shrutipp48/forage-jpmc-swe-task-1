2e61b69a70e157079071de12b1e143eea8f7ccb4
Shruti Parab <parabshrutiprasad@gmail.com>
2023-05-11 16:37:32 +0530

    final changes

--- client3.py
+++ client3.py
@@ -35,25 +35,32 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
-def getRatio(price_a, price_b):
+def getRatio(prices):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    price_a = prices.get("ABC")
+    price_b = prices.get("DEF")
+    if(price_a is not None and price_b is not None and price_b != 0):
+        return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {}
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        ratio = getRatio(prices)
+        if ratio is not None:
+            print("Ratio %s" % ratio)
--- client_test.py
+++ client_test.py
@@ -2,24 +2,44 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+          {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+          {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+
+        # Call getDataPoint() function and store the returned values
+        stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+
+        # Assert that the returned values are of the correct type
+        self.assertIsInstance(stock, str)
+        self.assertIsInstance(bid_price, float)
+        self.assertIsInstance(ask_price, float)
+        self.assertIsInstance(price, float)
+
+        # Assert that the returned value is equal to the average of the top bid and top ask prices
+        self.assertEqual(price, (121.2 + 120.48) / 2)
+
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+          {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+          {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+
+        stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+
+        self.assertIsInstance(stock, str)
+        self.assertIsInstance(bid_price, float)
+        self.assertIsInstance(ask_price, float)
+        self.assertIsInstance(price, float)
+
+        self.assertEqual(stock, "ABC")
+        self.assertEqual(bid_price, 120.48)
+        self.assertEqual(ask_price, 119.2)
+        self.assertEqual(price, (120.48 + 119.2) / 2)
 
 
-  """ ------------ Add more unit tests ------------ """
 
 
 
-if __name__ == '__main__':
-    unittest.main()
